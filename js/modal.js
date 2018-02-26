e8e2bcfc8cf195666e9a53cbb2e5d7d201157539
sss_xks <30371455+sssxks@users.noreply.github.com>
2018-02-26 14:31:06 +0800

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,18 @@
 function modal(remote,data){
 	var modalContent = $.get(remote,function(responseTxt,statusTxt,xhr){
 		if(statusTxt=="success"){
+			if (data != null) {
+				for (var i in data){
+					responseTxt.replace(new RegExp('$' + i + '$', 'g'),data[i]);
+				};
+			};
+			console.log(responseTxt);
+			$('.modal-content').html(responseTxt);
 			$('#modal').show();
 			$('body').css('overflow','hidden').css('padding-right','17px');
+			
 		} else if(statusTxt=="error"){
 			throw 'cannot load modal';
 		};
-	});
-	console.log(modalContent);
-	if (data != null) {
-		for (var i in data){
-			modalContent.replace(new RegExp('$' + i + '$', 'g'),data[i]);
-		};
-	};
-	$('.modal-content').html(modalContent);
+	});	
 }
