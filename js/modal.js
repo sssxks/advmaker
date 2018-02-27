8518e02e412817c71dd1ad7008adfe34585b04aa
sss_xks <30371455+sssxks@users.noreply.github.com>
2018-02-27 20:40:55 +0800

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -4,7 +4,7 @@ function modal(remote,data){
 			if (data != null) {
 				for (var i in data){
 					console.log('_' + i + ':' + data[i]);
-					console.log(responseTxt.split('_' + i))//.join(data[i]);
+					console.log(responseTxt.split('_' + i).join(data[i]));
 				};
 			};
 			console.log(typeof responseTxt);
