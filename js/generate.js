83405bbbd6ae5401c0832a2f23f6447b77ddedd4
sss_xks <30371455+sssxks@users.noreply.github.com>
2018-02-27 21:54:40 +0800

    Update generate.js

--- js/generate.js
+++ js/generate.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
 	$('.generate').click(function(){
-		modal('modal/generate.htm',{
+		var output = {
+				criteria:{}
+			};
+		if (document.getElementById("st_display").checked) {
+			output.display = {};
+			var title = document.getElementById("st_title").value
+			if (title == null || title == '') {
+				modal('modal/generate-error.htm',{
+					reason:'未输入标题'
+				})
+			}
+		var outputJSON = JSON.stringify(output);
+		modal('modal/generate-success.htm',{
 			path: 'dsa',
 			code: 'asd'});
 	});
