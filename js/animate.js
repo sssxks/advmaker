dbe3538bc9255cc0f3e6c37c534f55143eed5591
sss_xks <30371455+sssxks@users.noreply.github.com>
2018-02-27 12:30:19 +0800

    Update animate.js

--- js/animate.js
+++ js/animate.js
@@ -1,18 +1,20 @@
 $(document).ready(function(){
 	//加载动画
+	$('body').css('overflow','hidden').css('padding-right','17px');
 	$('#content').addClass('animated fadeInUp')
 		.css('-webkit-animation-duration','0.5s')
 		.css('animation-duration','0.5s')
 		.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
 			$(this).removeClass('animated fadeInUp');
 			$(this).attr('style','');
+			$('body').css('overflow','auto').css('padding-right','0');
 		});
 	$('#header').slideDown(450);
 	//提示框
 	$('body').tooltip({
 		track: true
 	});
-	
+	//切换开关
 	$('#st_display').click(function (){
 		//var value = document.getElementById('display_or_do_not').checked;
 		$('#c_display').toggle(350);
